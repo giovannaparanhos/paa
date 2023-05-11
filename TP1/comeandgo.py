8258c7a7271793c71ff54c4e397c437fcf7aa15b
Giovanna Paranhos <giovannaparanhos@gmail.com>
2023-05-11 17:43:18 -0300

    updating cag

--- TP1/comeandgo.py
+++ TP1/comeandgo.py
@@ -1,70 +1,43 @@
 from collections import defaultdict
 
-def build_testcases(v, testcase:str):
-    weights = build_matrix(v, 1) #equal weights
-    graph = build_graph(testcase)
-    #print(graph)
-    
-
-    #print(vertices)
-    print(ok)
-
 def build_matrix(n, value = 0):
     line = n*[value]
     matrix = n*[line]
     return matrix
 
-def build_graph(testcase):
-    for street in testcase:   
-        if len(street) == 2:
-            testcase = street
-            n = int(testcase[0])
-            m = int(testcase[1])    
-            graph = defaultdict(list)
-        if n == 0 and m ==0:
-            break
-        
-        if len(street) == 3:
-            # vertex v
-            v = street[0]
-            # vertex w
-            w = street[1]
-            #adjacency type
-            p = street[2]
-            # pair vw is one-way v->w if 1, 
-            # two-way v<=>w if 2
-            if p == 1:
-                graph[v].append(w)
-
-            elif p ==2:
-                if w not in graph[v]:
-                    graph[v].append(w)
-
-                if v not in graph[w]: 
-                    graph[w].append(v)
-            if w not in graph.keys():
-                graph[w] = []
-            continue
-    return dict(graph)
-
-
-    
 def floyd_warshall(W):
-    pass
+    n = len(W)
+    D_k_1 = [row[:] for row in W]
+    for k in range(0, n):
+        D_k = build_matrix(n)
+        for i in range(0, n):
+            for j in range(0, n):
+                D_k[i][j] = min(D_k_1[i][j], (D_k_1[i][k]+D_k_1[k][j]))
+                D_k[j][j] = 1
+        D_k_1 = D_k
+    return D_k_1
+def run_algo(W):
+    matrix = floyd_warshall(W)
+    for i in len(matrix):
+        if 0 in matrix:
+            return False
+    return True
 
-testcase = []
 while True:
     line = input()
     line = line.strip().split(' ')
     line = list(map(int, line)) 
     
     if len(line) == 2:
+        if adj_matrix:
+            run_algo(adj_matrix)
         n_vertices = line[0]
-        if len(testcase) > 1:        
-            build_testcases(testcase)
-            testcase = []
+        adj_matrix = build_matrix(n_vertices)
+    elif len(line) == 3:
+        #include lines
+        pass
     
     if line == [0, 0]: break
-    testcase.append(line)  
+
 
 
--- implementations/floyd_warshall.py
+++ implementations/floyd_warshall.py
@@ -13,10 +13,23 @@ def build_matrix(n:int, value = 0):
     return matrix
 
 def floyd_warshall(W):
+    ''''Implementation of the Floyd-Warshall algorithm.
+    Parameters
+    ----------
+    W: list(list)
+     Weight matrix
+    
+    Returns
+    -------
+    D_k_1: list(list)
+    '''
     n = len(W)
-    D_k_1 = W
-    for k in range(1, n+1):
+    D_k_1 = [row[:] for row in W]
+    for k in range(0, n):
         D_k = build_matrix(n)
-        for i in range(0, n+1):
-            for i in range(0, n+1):
+        for i in range(0, n):
+            for j in range(0, n):
+                D_k[i][j] = min(D_k_1[i][j], (D_k_1[i][k]+D_k_1[k][j]))
+        D_k_1 = D_k
+    return D_k_1
 
